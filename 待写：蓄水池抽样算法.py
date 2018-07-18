14b05ddc0405794a21651504d920ecf0e5429405
HuskyBin <xiang.zh@husky.neu.edu>
2018-07-17 23:26:17 -0400

    Update and rename 待写：蓄水池抽样算法 to 待写：蓄水池抽样算法.py

---
+++ 待写：蓄水池抽样算法.py
@@ -0,0 +1,22 @@
+很容易能够想到这样一个简单的式子来凑出1/n：
+
+pi=1i×ii+1×i+1i+2×⋯×n−1n=1n
+
+class RandomSelector:
+  def __init__(self, rand=None):
+    self._selectedItem = None
+    self._count = 0
+    self._rand = rand
+    if self._rand is None:
+      self._rand = Random()
+
+  def SelectedItem(self):
+    return self._selectedItem
+
+  def Count(self):
+    return self._count
+
+  def AddItem(self, item):
+    if self._rand.randint(0, self._count) == 0:
+      self._selectedItem = item
+    self._count += 1
