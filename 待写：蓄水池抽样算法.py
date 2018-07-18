64559bea4172b6138d87d7bfd23d73ea115d99c3
HuskyBin <xiang.zh@husky.neu.edu>
2018-07-17 23:33:22 -0400

    Update 待写：蓄水池抽样算法.py

--- 待写：蓄水池抽样算法.py
+++ 待写：蓄水池抽样算法.py
@@ -19,3 +19,22 @@ class RandomSelector:
     if self._rand.randint(0, self._count) == 0:
       self._selectedItem = item
     self._count += 1
+
+
+C++:
+class Solution {
+public:
+    Solution(vector<int> nums): v(nums) {}
+    
+    int pick(int target) {
+        int cnt = 0, res = -1;
+        for (int i = 0; i < v.size(); ++i) {
+            if (v[i] != target) continue;
+            ++cnt;
+            if (rand() % cnt == 0) res = i;
+        }
+        return res;
+    }
+private:
+    vector<int> v;
+};
