3331d58a21aa0ed814789a3c4caf9a7895cdee4c
HuskyBin <xiang.zh@husky.neu.edu>
2019-02-19 22:55:42 -0500

    Update Reservoir Sampling.java

--- Reservoir Sampling.java
+++ Reservoir Sampling.java
@@ -3,12 +3,12 @@
 // 第二个  (1 - 1/n) * 1/(n - 1) = 1 /n
 / ...
 public int[] shuffle() {
-        Random random = new Random();
-        for (int i = num.length; i > 1; i--) {
-            int nextRandom = random.nextInt(i);
-            int temp = num[i - 1];
-            num[i - 1] = num[nextRandom];
-            num[nextRandom] = temp;
-        }
-        return num;
-    }
+    Random random = new Random();
+    for (int i = num.length; i > 1; i--) {
+         int nextRandom = random.nextInt(i);
+         int temp = num[i - 1];
+         num[i - 1] = num[nextRandom];
+         num[nextRandom] = temp;
+     }
+     return num;
+}   
