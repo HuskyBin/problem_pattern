16807ca02fabcce4d7b73690433f2072f456e48d
HuskyBin <xiang.zh@husky.neu.edu>
2019-10-03 00:41:00 -0400

    Update Sliding window.java

--- Sliding window.java
+++ Sliding window.java
@@ -254,10 +254,10 @@ public class Solution {
             while(counter == 0){
                 char tempc = s.charAt(begin);
                 if(map.containsKey(tempc)){
-                    map.put(tempc, map.get(tempc) + 1);
-                    if(map.get(tempc) > 0){
+                    if(map.get(tempc) == 0){
                         counter++;
                     }
+                    map.put(tempc, map.get(tempc) + 1);
                 }
                 if(end-begin == t.length()){
                     result.add(begin);
