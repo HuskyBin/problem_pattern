6e2b0320e4cc62d9cd1fbbbd76aa7f542f350e0e
HuskyBin <xiang.zh@husky.neu.edu>
2019-10-20 17:54:38 -0400

    Create quick_sort.java

---
+++ quick_sort.java
@@ -0,0 +1,43 @@
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return Integer.MIN_VALUE;
+        }       
+        int result = findKthLargestCore(nums, 0, nums.length - 1, nums.length - k + 1);
+        return result;
+    }
+
+    private int findKthLargestCore(int[] nums, int start, int end, int k) {
+        int left = start;
+        int right = end;
+        int pivot = nums[end];
+
+        while (left != right) {
+            while (nums[left] < pivot && left < right) {
+                left++;
+            }
+            while (nums[right] >= pivot && left < right) {
+                right--;
+            }
+
+            swap(nums, left, right);
+        }
+        swap(nums, left, end);
+
+        if (k == left + 1) {
+            return pivot;
+        }
+        else if (k < left + 1) {
+            return findKthLargestCore(nums, start, left - 1, k);
+        }
+        else {
+            return findKthLargestCore(nums, left + 1, end, k);
+        }
+    }
+
+    public void swap(int[] nums, int aIndex, int bIndex) {
+        int temp = nums[aIndex];
+        nums[aIndex] = nums[bIndex];
+        nums[bIndex] = temp;
+    }
+}
