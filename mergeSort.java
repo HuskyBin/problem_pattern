2db70c6001e5a1618666a50b59c4edd936e0ba15
HuskyBin <xiang.zh@husky.neu.edu>
2019-10-19 23:45:12 -0400

    Update mergeSort.java

--- mergeSort.java
+++ mergeSort.java
@@ -1,3 +1,5 @@
+https://leetcode.com/problems/count-of-smaller-numbers-after-self/discuss/138154/The-C%2B%2B-merge-sort-template-for-pairs-'i'-'j'-problem
+
 Merge Sort
 Merge Sort is suitable for problems which look for some pairs i, j
 such that i < j, and nums[i], nums[j] satisfy some constraints.
