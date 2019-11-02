a978cb05bdf590a6e4c2bbaf3165d610ef77c937
HuskyBin <xiang.zh@husky.neu.edu>
2019-11-02 01:56:06 -0400

    Update A*搜索.java

--- A*搜索.java
+++ A*搜索.java
@@ -33,3 +33,57 @@ public int cutOffTree(List<List<Integer>> forest, int sr, int sc, int tr, int tc
     }
     return -1;
 }
+
+
+//1197. Minimum Knight Moves
+class Solution {
+    class Triplet{//stores node information, g_cost(depth), and f_cost(heuristic_cost+g_cost)
+        int[] node; 
+        int g_cost, f_cost;
+        Triplet(int[] node, int g, int f){
+            this.node=node;
+            this.g_cost=g;
+            this.f_cost=f; 
+        }
+    }
+    private static final int[][] dir = {{-1,-2},{1,-2},{-1,2},{1,2},{-2,-1},{2,-1},{-2,1},{2,1}};
+    public int minKnightMoves(int x, int y) {
+        //reduce search space
+        x = Math.abs(x); y = Math.abs(y);
+        //store seen cells
+        Set<String> seen = new HashSet();
+    	seen.add(0+","+0);
+        //use a priorityqueue and sort by f(n)
+    	PriorityQueue<Triplet> pq = new PriorityQueue<Triplet>((Triplet a1, Triplet a2)->a1.f_cost-a2.f_cost);
+    	pq.add(new Triplet(new int[] {0,0}, 0, heursitic_cost(x, y, 0, 0)));
+    	while(!pq.isEmpty()) {
+    		Triplet cur = pq.poll();
+    		int[] node = cur.node;
+    		int depth = cur.g_cost;
+    		if(node[0]==x && node[1]==y) {//if arrive at destination, return g(n)
+    			return depth;
+    		}
+    		int next_depth = depth+1;
+    		for(int j=0; j<dir.length; ++j){//check neighbors
+    			int nxt_x = node[0]+dir[j][0];
+                int nxt_y = node[1]+dir[j][1];
+                String nxt = nxt_x+","+nxt_y;
+                if(!seen.contains(str) && nxt_x>=-2 && nxt_y>=-2){//here we can add a judging statement if there's obstacle
+                	int h_cost = heursitic_cost(x, y, nxt_x, nxt_y);
+                	pq.add(new Triplet(new int[] {nxt_x, nxt_y}, next_depth, next_depth+h_cost));
+                	seen.add(nxt);
+                }
+    		}
+    	}
+    	return 0;
+    }
+    
+    private int heursitic_cost(int x, int y, int x1, int y1) {//admissible, never overestimate the cost
+    	int max=Math.max(Math.abs(x-x1), Math.abs(y-y1));
+    	int min=Math.min(Math.abs(x-x1), Math.abs(y-y1));
+    	int short_cost = Math.max(0, (min*2-max)/3);
+    	int long_cost = (max-short_cost)/3;
+ 
+    	return long_cost+short_cost;
+    }
+}
